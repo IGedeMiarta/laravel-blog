2f35d7c097426c025e2353e9c01f2d9983fbcdff
miarta <miarta.igede@gmail.com>
2021-09-19 19:39:02 +0800

    factory & faker v10 git commit -m factory

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\PostController;
 use App\Models\Category;
 use App\Models\Post;
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -33,17 +34,23 @@ Route::get('/about', function () {
 Route::get('/blog', [PostController::class,'index']);
 
 // halaman single post
-Route::get('posts/{post:slug}', [PostController::class,'show']);
-Route::get('categories', function () {
+Route::get('/posts/{post:slug}', [PostController::class,'show']);
+Route::get('/categories', function () {
     return view('categories',[
         'title' => 'Post Categories',
         'categories'=> Category::all()
     ]);
 });
-Route::get('categories/{category:slug}', function (Category $category) {
+Route::get('/categories/{category:slug}', function (Category $category) {
     return view('category',[
         'title' => $category->name,
         'posts' => $category->posts,
         'category'=>$category->name
+    ]);
+});
+Route::get('/authors/{author:username}',function(User $author){
+    return view('posts',[
+        'title' => 'User Post',
+        'posts' => $author->posts,
     ]);
 });
