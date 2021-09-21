3316fd7dc90e59a25989fa9437ab454722e9dedc
miarta <miarta.igede@gmail.com>
2021-09-21 22:34:15 +0800

    login & middleware

--- resources/views/login/index.blade.php
+++ resources/views/login/index.blade.php
@@ -11,14 +11,33 @@
                     </button>
                 </div>
             @endif
+            @if (session()->has('loginError'))
+                <div class="alert alert-danger alert-dismissible fade show mt-5" role="alert">
+                    {{ session('loginError') }}
+                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+            @endif
             <main class="form-signin">
                 <h1 class="h3 mb-3 fw-normal text-center ">Please Login</h1>
-                <form>
+                <form action="/login" method="POST">
+                    @csrf
                     <div class="form-floating">
-                        <input type="email" class="form-control" id="floatingInput" placeholder="Username">
+                        <input type="email" name="email"
+                            class="form-control @error('email')
+                            is-invalid
+                        @enderror"
+                            id="email" placeholder="Email" autofocus required value="{{ old('email') }}">
+                        @error('email')
+                            <div class="invalid-feedback">
+                                {{ $message }}
+                            </div>
+                        @enderror
                     </div>
                     <div class="form-floating">
-                        <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+                        <input type="password" name="password" class="form-control" id="password" placeholder="Password"
+                            required>
                     </div>
                     <button class="w-100 btn btn-lg btn-primary" type="submit">login</button>
                     <small class="d-block text-center">Not Registred? <a href="/register">Regist now</a></small>
--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -21,11 +21,39 @@
                          href="/categories">Categories</a>
                  </li>
              </ul>
+
              <ul class="navbar-nav ms-auto">
-                 <li class="nav-item">
-                     <a class="nav-link {{ $active === 'login' ? 'active' : '' }}" href="/login"><i
-                             class="bi bi-box-arrow-in-right"></i> Login</a>
+                 @auth
+                     <li class="nav-item dropdown">
+                         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                             data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                             Hello {{ auth()->user()->name }}
+                         </a>
+                         <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                             <li><a class="dropdown-item" href="#"><i class="bi bi-layout-sidebar-inset"></i> Dashboard</a>
+                             </li>
+                             <li>
+                                 <div class="dropdown-divider"></div>
+                             </li>
+                             <li>
+                                 <form action="/logout" method="POST">
+                                     @csrf
+                                     <button type="submit" class="dropdown-item"><i class="bi bi-box-arrow-right"></i> Log
+                                         out</button>
+
+                                 </form>
+                             </li>
+                         </ul>
+                     </li>
+                 @else
+                     <li class="nav-item">
+                         <a class="nav-link {{ $active === 'login' ? 'active' : '' }}" href="/login"><i
+                                 class="bi bi-box-arrow-in-right"></i> Login</a>
+                     </li>
+                 @endauth
              </ul>
+
+
          </div>
      </div>
  </nav>
